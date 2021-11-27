0191b854d63c6be85293ea12e4437c77211daaf4
Peter Renner <peter_renner@Peters-MacBook-Pro.local>
2021-11-27 00:19:01 -0600

    Added baseline word stats in whole numbers. Got rid of words with '-'. Added testing software

--- python/getWordStats.py
+++ python/getWordStats.py
@@ -1,7 +1,13 @@
- import json
+import json
 
+print("Takes list of words and logs patterns into a json file")
+
+print("Please specify input file name. Do not include .json extension in name. It is assumed by the program.")
+inputFile = input() + ".json"
+
+
+# Initializes main dictionary (used in resulting json file)
 wordStats = {}
-
 for i in range(97, 123):
 	wordStats[chr(i)] = {
 		"startsWord": 0,
@@ -9,7 +15,21 @@ for i in range(97, 123):
 		"endsWord": 0,
 	}
 
-with open("internet_words.json") as words	
+with open(inputFile) as in_file:
+	words = json.load(in_file)
+	for word in words:
+		firstLetter, lastLetter = word[0], word[-1]
+		wordStats[firstLetter]["startsWord"] += 1
+		wordStats[lastLetter]["endsWord"] += 1
+		print(word)
+		if len(word) > 1:
+			for i in range(0, len(word) - 1):
+				letter = word[i]
+				letterAfter = word[i + 1]
+				try:
+					wordStats[letter]["comesBefore"][letterAfter] += 1
+				except:
+					wordStats[letter]["comesBefore"][letterAfter] = 1
 
 with open("wordStats.json", "w") as out_file:
 	json.dump(wordStats, out_file, indent = 2)
