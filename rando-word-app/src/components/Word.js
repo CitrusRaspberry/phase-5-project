878500865bf38bf65eb7d4a0df392f83220ed783
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2021-12-02 00:31:36 -0600

    Created first letter generating and word generating functions.

--- rando-word-app/src/components/Word.js
+++ rando-word-app/src/components/Word.js
@@ -1,8 +1,8 @@
 import { useState, useEffect } from "react";
 
 function Word() {
-    let word = "hi";
-    const [wordStats, setWordStats] = useState({});
+    const [ wordStats, setWordStats ] = useState({});
+    const [ generatedWord, setGeneratedWord ] = useState("loading...");
 
     useEffect(() => {
         fetch("http://localhost:3001/data")
@@ -10,43 +10,54 @@ function Word() {
         .then(setWordStats)
     }, []);
 
-    function getRandomLetter(object, placement) {
-        // placement argument can equal "startsWord", "endsWord", or can be any
-        // lowercase letter if in the middle of a word
-            // iterateObj = object[placement].comesBefore
-        let breakpoint = 0;
-        let ranNum = Math.random()
-        console.log(ranNum);
-        for (let letter in object) {
-            breakpoint += (object[letter][placement] ? object[letter][placement].ratio : object[letter].ratio);
-            console.log(letter, breakpoint);
-            if (ranNum < breakpoint) {
-                console.log("LETTER -->", letter);
-                return letter;
+    useEffect(() => {
+        function getRandomLetter(wordStats, placement) {
+            // placement argument can equal "startsWord", "endsWord", or can be blank
+            // lowercase letter if in the middle of a word
+            const placementIsLetter = placement.length === 1;
+            const stats = (placementIsLetter ? wordStats[placement].comesBefore : wordStats)
+            let breakpoint = 0;
+            const ranNum = Math.random();
+            console.log(ranNum);
+            for (let letter in stats) {
+                breakpoint += (placementIsLetter
+                    ? stats[letter].ratio
+                    : stats[letter][placement].ratio);
+                console.log(placement, letter, breakpoint);
+                if (ranNum < breakpoint) {
+                    return letter;
+                }
             }
         }
-    }
-    function testRandomLetter() {
-        let breakpoint = 0;
-        const ranNum = Math.random()
-        console.log(ranNum);
-        for (let letter in wordStats) {
-            breakpoint += wordStats[letter].startsWord.ratio;
-            console.log(letter, breakpoint);
-            if (ranNum < breakpoint) {
-                ranNum = 2;
-                console.log("LETTER -->", letter);
+        function createWord(numOfLetters) {
+            let word = "";
+            for ( let i = 0; i < numOfLetters; i++ ) {
+                switch ( i ) {
+                    case 0:
+                        word += getRandomLetter(wordStats, "startsWord")
+                        console.log("Starting letter -->", word);
+                        break;
+                    case numOfLetters - 1:
+                        word += getRandomLetter(wordStats, "endsWord")
+                        console.log("Ending letter -->", word.slice(-1));
+                        break;
+                    default:
+                        const prevLetter = word.slice(-1);
+                        word += getRandomLetter(wordStats, prevLetter)
+                        console.log("Mid letter -->", word.slice(-1));
+                }
             }
+            return word;
         }
-    }
+        if (Object.keys(wordStats).length) {
+            setGeneratedWord(createWord(5))
+        }
+    }, [wordStats])
 
-    if (Object.keys(wordStats).length) {
-        getRandomLetter(wordStats.a.comesBefore, "endsWord")
-    }
 
     return (
         <h2>
-            {word}
+            {generatedWord}
         </h2>
     )
 }
