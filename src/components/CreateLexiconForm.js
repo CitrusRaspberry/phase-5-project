1cd40932261ba0aac28e49dafed928c5a61d5630
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2022-01-04 18:07:52 -0600

    Added new, better looking alerts to the Create Lexicon Form and optimized code in that component

--- src/components/CreateLexiconForm.js
+++ src/components/CreateLexiconForm.js
@@ -6,18 +6,25 @@ import {
     Paper,
     Container,
     List,
+    Alert,
+    AlertTitle,
+    Stack,
 } from "@mui/material";
-import UploadIcon from '@mui/icons-material/Upload';
+import UploadIcon from "@mui/icons-material/Upload";
 import LoadingButton from "@mui/lab/LoadingButton";
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 import ValidityItem from "./ValiditiyItem";
 
 function CreateLexiconForm() {
-    const [ allLexiconNames, setAllLexiconNames ] = useState([]);
-    console.log(allLexiconNames);
-    const [ isLoading, setIsLoading ] = useState(false);
-    const [ errors, setErrors ] = useState({
+    const [showAlert, setShowAlert] = useState({
+        success: false,
+        failure: false,
+    });
+    const [allLexiconNames, setAllLexiconNames] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [errors, setErrors] = useState({
         lexiconName: {
             blank: true,
             inUse: false,
@@ -28,20 +35,20 @@ function CreateLexiconForm() {
             json: true,
         },
     });
-    const [ formData, setFormData ] = useState({
+    const [formData, setFormData] = useState({
         lexiconName: "",
         fileData: null,
     });
-    const anyNameErrors = Object.values(errors.lexiconName).find(er => er);
-    const anyFileErrors = Object.values(errors.fileData).find(er => er);
+    const anyNameErrors = Object.values(errors.lexiconName).find((er) => er);
+    const anyFileErrors = Object.values(errors.fileData).find((er) => er);
     const handleNameChange = (e) => {
         const value = e.target.value.toLowerCase();
         setFormData({
             ...formData,
             [e.target.name]: value,
         });
-        const valueIsValid = value.search(/[^a-zA-Z0-9-_]/) === -1
-        const valueIsUnique = allLexiconNames.indexOf(value) < 0
+        const valueIsValid = value.search(/[^a-zA-Z0-9-_]/) === -1;
+        const valueIsUnique = allLexiconNames.indexOf(value) < 0;
         setErrors({
             ...errors,
             lexiconName: {
@@ -50,6 +57,12 @@ function CreateLexiconForm() {
                 notValid: !valueIsValid,
             },
         });
+        if (showAlert.success) {
+            setShowAlert(() => ({
+                ...showAlert,
+                success: false
+            }))
+        }
     };
     // console.log("DATA", formData)
     const handleFileChange = (e) => {
@@ -57,21 +70,18 @@ function CreateLexiconForm() {
         const reader = new FileReader();
         reader.onload = () => {
             let data;
-            let failure;
-            const failValidation = (message, cause) => {
-                message && alert(message);
-                data = null;
-                failure = cause;
-            };
+            let cause;
+            let message;
             const fn = file.name;
             const fileExt = fn.slice(fn.search(/\.[a-z]+$/i)).toLowerCase();
             switch (fileExt) {
                 case ".csv":
                     const csv = reader.result;
                     if (csv.search(",") >= 0) {
-                        failValidation(
-                            "CSV file must not contain any commas, because all words should be contained in a single column.", "csv"
-                        );
+                        message =
+                            "CSV file must not contain any commas, because all words should be contained in a single column.";
+                        data = null;
+                        cause = "csv";
                         break;
                     }
                     data = csv.split("\n").filter((w) => w !== "");
@@ -79,45 +89,49 @@ function CreateLexiconForm() {
                 case ".json":
                     const parsedJSON = JSON.parse(reader.result);
                     if (!(parsedJSON instanceof Array)) {
-                        failValidation(
-                            "JSON file must contain an array/list, not an object/hash/dictionary.", "json"
-                        );
+                        message =
+                            "JSON file must contain an array/list, not an object/hash/dictionary.";
+                        data = null;
+                        cause = "json";
                         break;
                     } else if (parsedJSON.find((l) => l instanceof Object)) {
-                        failValidation(
-                            "JSON file contents must only be one layer deep.", "json"
-                        );
+                        message =
+                            "JSON file contents must only be one layer deep.";
+                        data = null;
+                        cause = "json";
                         break;
                     }
                     data = parsedJSON.filter((w) => w !== "");
                     break;
                 default:
-                    failValidation(
-                        `Only .json and .csv files are accepted. You uploaded a ${fileExt} file type.`
-                    );
+                    message = `Only .json and .csv files are accepted. You uploaded a ${fileExt} file type.`;
                     break;
             }
-            const extIsValid = fileExt === ".csv" || fileExt === ".json";
-            const fileAddress = extIsValid ? undefined : "";
-            setFormData({
+            setFormData(() => ({
                 ...formData,
                 fileData: data,
-            });
-            setErrors({
+            }));
+            setErrors(() => ({
                 ...errors,
                 fileData: {
-                    [failure]: !data,
+                    [cause]: !data,
                 },
-            });
+            }));
+            setShowAlert(() => ({
+                success: false,
+                failure: message,
+            }));
         };
         reader.readAsText(file);
     };
     const handleSubmit = (e) => {
         e.preventDefault();
         if (anyFileErrors || anyNameErrors) {
-            alert("You must fill out all fields and resolve all errors before submitting.");
+            alert(
+                "You must fill out all fields and resolve all errors before submitting."
+            );
         } else {
-            setIsLoading(true)
+            setIsLoading(true);
             const body = {
                 name: formData.lexiconName,
                 words: formData.fileData,
@@ -131,40 +145,66 @@ function CreateLexiconForm() {
                 body: JSON.stringify(body),
             };
             fetch("https://word-generator-app.herokuapp.com/lexicons", config)
-            .then((r) => r.json())
-            .then((data) => {
-                setFormData(() => ({
-                    ...formData,
-                    lexiconName: "",
-                }));
-                setIsLoading(() => false)
-            }).catch((error) =>
-                console.log(
-                    "POST error when creating new lexicon... ==>",
-                    error
-                )
-            );
+                .then((r) => r.json())
+                .then((data) => {
+                    setFormData(() => ({
+                        ...formData,
+                        lexiconName: "",
+                    }));
+                    setErrors(() => ({
+                        ...errors,
+                        lexiconName: {
+                            ...errors.lexiconName,
+                            blank: true,
+                        },
+                    }));
+                    setIsLoading(() => false);
+                    setShowAlert(() => ({
+                        success: true,
+                        failure: false,
+                    }));
+                })
+                .catch((error) =>
+                    console.log(
+                        "POST error when creating new lexicon... ==>",
+                        error
+                    )
+                );
         }
     };
     useEffect(() => {
         fetch("https://word-generator-app.herokuapp.com/lexicons")
-        .then(r => r.json())
-        .then(data => setAllLexiconNames(data.map(l => l.name)))
-        .catch(error => console.log("Could not grab all lexicons from server...",  error))
-    }, [])
+            .then((r) => r.json())
+            .then((data) => setAllLexiconNames(data.map((l) => l.name)))
+            .catch((error) =>
+                console.log("Could not grab all lexicons from server...", error)
+            );
+    }, []);
     return (
         <form onSubmit={handleSubmit}>
             <Paper elevation={12}>
                 <Container>
                     <Grid container spacing={8}>
+                        {showAlert.success && (
+                            <Grid item xs={12}>
+                                <Stack
+                                    sx={{ width: "100%", textAlign: "left" }}
+                                    spacing={2}
+                                >
+                                    <Alert severity="success">
+                                        Upload to the database was successful! See it on the <Link to="/community-lexicons">Community Lexicons</Link> page.
+                                    </Alert>
+                                </Stack>
+                            </Grid>
+                        )}
+
                         <Grid item xs={12} md={6}>
                             <TextField
                                 name="lexiconName"
                                 label="Lexicon Name"
                                 variant="filled"
                                 helperText={
-                                    anyNameErrors &&
-                                    "Name is not valid"
+                                    anyNameErrors && "Name is not valid"
                                 }
                                 onChange={(e) => handleNameChange(e)}
                                 value={formData.lexiconName}
@@ -172,25 +212,37 @@ function CreateLexiconForm() {
                                 fullWidth
                             />
                             <List>
-                                <ValidityItem 
+                                <ValidityItem
                                     text="Name cannot be blank."
                                     isValid={!errors.lexiconName.blank}
                                 />
-                                <ValidityItem 
+                                <ValidityItem
                                     text="Name cannot already be in use."
                                     isValid={!errors.lexiconName.inUse}
                                 />
-                                <ValidityItem 
+                                <ValidityItem
                                     text="Only valid characters are letters, numbers, '-', and '_'."
                                     isValid={!errors.lexiconName.notValid}
                                 />
                             </List>
                         </Grid>
                         <Grid item xs={12} md={6}>
-                            <InputLabel>
-                                Please choose a CSV or json file containing
-                                words
-                            </InputLabel>
+                            {showAlert.failure ? (
+                                <Stack
+                                    sx={{ width: "100%", textAlign: "left" }}
+                                    spacing={2}
+                                >
+                                    <Alert severity="error">
+                                        <AlertTitle>File Error</AlertTitle>
+                                        {showAlert.failure}
+                                    </Alert>
+                                </Stack>
+                            ) : (
+                                <InputLabel>
+                                    Please choose a CSV or json file containing
+                                    words
+                                </InputLabel>
+                            )}
                             <Input
                                 name="file"
                                 label="Upload File"
@@ -199,15 +251,13 @@ function CreateLexiconForm() {
                                 error={anyFileErrors}
                                 fullWidth
                             />
-                            <InputLabel>
-                                File upload check-list:
-                            </InputLabel>
+                            <InputLabel>File upload check-list:</InputLabel>
                             <List>
-                                <ValidityItem 
+                                <ValidityItem
                                     text="For CSV, use one column with no headers."
                                     isValid={!errors.fileData.csv}
                                 />
-                                <ValidityItem 
+                                <ValidityItem
                                     text="For json, let the structure be an array only
                                     a single layer deep."
                                     isValid={!errors.fileData.json}
